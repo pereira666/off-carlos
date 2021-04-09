0994e907408f121212a78cb3fa303584883a8cc0
pereira666 <antcarlospereiral@gmail.com>
2021-04-09 14:34:26 -0300

    First commit

---
+++ src/daftarvip.js.bak
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 2 > Acessar recursos ViP
+-Rp. 5 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/558499034946 ou digite *${prefix}dono*_
+
+*NOTA*
+
+*GRUPO DO LOSER 🐊🚩 :*
+_E M - B R E V E_ `
+}
+exports.daftarvip = daftarvip
---
+++ src/help.js
@@ -0,0 +1,254 @@
+const help = (prefix) => {
+	return `*_🔥L̷O̷S̷E̷R̷ ̷U̷L̷T̷I̷M̷A̷T̷E̷🔥_*
+╔─━━━━━━░   •    ━━━━━━─╗
+✿ 𝚂𝚃𝙰𝚃𝚄𝚂: ONLINE
+✿ ᴅᴏɴᴏ: CARLOSX - FLEXX
+✿ sᴜᴘᴏʀᴛᴇ: wa.me/557588265283
+
+ ╚─━━━━━━░   •    ━━━━━━─╝
+ 
+꧁ ᴍᴇɴᴜ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}figu* (normal/animadas)
+║➵ *${prefix}toimg* (converte figurinha em Imagem)
+║➵ *${prefix}info*
+║➵ *${prefix}dono*
+║➵ *${prefix}bug [sua mensagem]*
+║➵ *${prefix}ytmp4* (url)
+║➵ *${prefix}play* (procurar uma musica com o título)
+║➵ *${prefix}tomp3*
+║➵ *${prefix}mascote*
+║➵ *${prefix}assttp* (texto)
+║➵ *${prefix}darkjokes (memes aleatórios)*
+║➵ *${prefix}estudar* (motivacional)
+║➵ *${prefix}tts* (idioma) (texto)
+║➵ *${prefix}loli*
+║➵ *${prefix}url2img*
+║➵ *${prefix}leens [na legenda]*
+║➵ *${prefix}wait [na legenda]*
+║➵ *${prefix}setprefix*
+║
+꧁ ᴏᴜᴛʀᴏs ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}linkgp*
+║➵ *${prefix}simih [1/0]* (temporariamente off)
+║➵ *${prefix}marcar*
+║➵ *${prefix}add [@]*
+║➵ *${prefix}banir [@]*
+║➵ *${prefix}promover [@]*
+║➵ *${prefix}rebaixar*
+║➵ *${prefix}admins*
+║➵ *${prefix}marcar2*
+║➵ *${prefix}bc [texto]* (ele faz uma ™)
+║➵ *${prefix}marcar3*
+║➵ *${prefix}bloqueados*
+║➵ *${prefix}bloquear [@]*
+║➵ *${prefix}desbloquear [@]*
+║➵ *${prefix}limpar* (apenas dono do bot)
+║➵ *${prefix}bc [ *texto* ]* (apenas dono do bot)
+║➵ *${prefix}bemvindo [1/0]*
+║➵ *${prefix}clonar [@]*
+║➵ *${prefix}dono*
+║➵ *${prefix}setnome*
+║➵ *${prefix}termux*
+║➵ *${prefix}setfoto*
+║➵ *${prefix}grupoinfo*
+║➵ *${prefix}marcar*
+║➵ *${prefix}marcar2*
+║➵ *${prefix}marcar3*
+║
+꧁ ᴅɪᴠᴇʀsᴀᴏ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}roleta*
+║➵ *${prefix}gay* [@]
+║➵ *${prefix}gay1* [@]
+║➵ *${prefix}gado*
+║➵ *${prefix}dado*
+║➵ *${prefix}casal*
+║➵ *${prefix}punheteiro* (aleatório)
+║➵ *${prefix}feio* (aleatório)
+║➵ *${prefix}lindo* (aleatório)
+║➵ *${prefix}gostoso* (aleatório)
+║➵ *${prefix}xingar* [@]
+║➵ *${prefix}pergunta* [Marque] e pergunte [lindo]
+║
+꧁ ɪᴍᴀɢᴇɴs ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}hentai*
+║➵ *${prefix}pe*
+║➵ *${prefix}buceta*
+║➵ *${prefix}tadas* (cantadas aleatórias)
+║➵ *${prefix}naruto*
+║➵ *${prefix}sasuke*
+║➵ *${prefix}minato*
+║➵ *${prefix}loli*
+║➵ *${prefix}miku*
+║➵ *${prefix}boanoite*
+║➵ *${prefix}bomdia*
+║➵ *${prefix}boatarde*
+║➵ *${prefix}mia* (off)
+║➵ *${prefix}wallpaper*
+║➵ *${prefix}rize [aleatórias]*
+║➵ *${prefix}minato [aleatórias]*
+║➵ *${prefix}lofi*
+║➵ *${prefix}malkova* 🔞
+║➵ *${prefix}nsfwloli* 🔞
+║➵ *${prefix}nsfwloli1* 🔞
+║➵ *${prefix}belle* 🔞
+║➵ *${prefix}randomhentai* 🔞
+║➵ *${prefix}reislin*
+║
+꧁ sᴏ ᴘʀᴇᴍɪᴜᴍ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}cekvip*
+║➵ *${prefix}premiumlist*
+║➵ *${prefix}delete*
+║➵ *${prefix}modapk*
+║➵ *${prefix}indo10*
+║➵ *${prefix}daftarvip [para virar Premium]*
+║➵ *${prefix}qrcode*
+║➵ *${prefix}chentai*
+║➵ *${prefix}gcpf*
+║➵ *${prefix}gbin*
+║➵ *${prefix}pack*
+║➵ *${prefix}destrava*
+║➵ *${prefix}gpessoa*
+║
+꧁ ɢʀᴜᴘᴏ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}banir*
+║➵ *${prefix}leveling [on/off]*
+║➵ *${prefix}level*
+║➵ *${prefix}add*
+║➵ *${prefix}promover*
+║➵ *${prefix}setfoto [na legenda]*
+║➵ *${prefix}setname [texto]*
+║➵ *${prefix}rebaixar*
+║➵ *${prefix}admins*
+║➵ *${prefix}marcar*
+║➵ *${prefix}marcar2*
+║➵ *${prefix}marcar3*
+║➵ *${prefix}bemvindo [1/0]*
+║➵ *${prefix}grupoinfo*
+║➵ *${prefix}bomdia*
+║➵ *${prefix}boatarde*
+║➵ *${prefix}boanoite*
+║➵ *${prefix}setdesc*
+║
+꧁ ᴜsᴇʀ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}ping [ver o ping do bot]*
+║➵ *${prefix}wame* (fazer seu número virar link)
+║➵ *${prefix}pinterest (nome)*
+║➵ *${prefix}desligar (so para o dono)*
+║➵ *${prefix}timer*
+║
+꧁ ᴏᴜᴛʀᴏs /2 ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}neko*
+║➵ *${prefix}testime*
+║➵ *${prefix}modoanime [on/off]*
+║➵ *${prefix}modonsfw [on/off]*
+║➵ *${prefix}rize*
+║➵ *${prefix}nomegp*
+║➵ *${prefix}darkjokes (memes aleatórios)*
+║➵ *${prefix}next*
+║➵ *${prefix}alerta*
+║➵ *${prefix}belle [img aleatórias]*
+║➵ *${prefix}hobby*
+║
+꧁ ᴇᴅɪᴛs ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}macarena* (créditos ao tio_marshytt)
+║➵ *${prefix}portugal*
+║➵ *${prefix}botada*
+║
+꧁ xxxᴛᴇɴᴛᴀᴛɪᴄᴏɴ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}xxxedit*
+║➵ *${prefix}xxx1*
+║➵ *${prefix}xxx2*
+║➵ *${prefix}xxx3*
+║
+꧁ ᴍᴜsɪᴄ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}m4*
+║➵ *${prefix}cmflamengo*
+║➵ *${prefix}blaze*
+║➵ *${prefix}teto1*
+║➵ *${prefix}teto2*
+║➵ *${prefix}teto3* 
+║➵ *${prefix}apagatudo* 
+║
+꧁ ᴄᴏᴍᴀɴᴅᴏs ᴅᴇ ᴠᴏᴢ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}falsidade*
+║➵ *${prefix}docinho*
+║➵ *${prefix}cirilo*
+║➵ *${prefix}nobru*
+║➵ *${prefix}ola*
+║➵ *${prefix}tchau*
+║➵ *${prefix}bem*
+║➵ *${prefix}onich*
+║➵ *${prefix}beat1*
+║➵ *${prefix}glub*
+║➵ *${prefix}gemidao*
+║
+꧁ ɪɴᴛᴇʀᴀᴛɪᴠᴏ ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+❗NOTA »
+Mandar a msg sem o *.*
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *oii*
+║➵ *bv*
+║➵ *canta ai bot*
+║➵ *grita*
+║➵ *fdp*
+║➵ *boanoite*
+║
+꧁ ᴅᴀɴᴄᴇ ᴛɪᴋᴛᴏᴋʀᴇs ꧂
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+║➵ *${prefix}tiktok1*
+║➵ *${prefix}tiktok2*
+║➵ *${prefix}tiktok3*
+║➵ *${prefix}tiktok4*
+║➵ *${prefix}tiktok5*
+║➵ *${prefix}tiktok6*
+║➵ *${prefix}tiktok7*
+║➵ *${prefix}tiktok8*
+║➵ *${prefix}tiktok9*
+║➵ *${prefix}tiktok10*
+║➵ *${prefix}tiktok11*
+║➵ *${prefix}tiktok12*
+║➵ *${prefix}tiktok13*
+║➵ *${prefix}tiktok14*
+║➵ *${prefix}tiktok15*
+║➵ *${prefix}tiktok16*
+║➵ *${prefix}tiktok17*
+║➵ *${prefix}tiktok18*
+║➵ *${prefix}tiktok19*
+║➵ *${prefix}tiktok20*
+║➵ *${prefix}tiktok21*
+║➵ *${prefix}tiktok22*
+║➵ *${prefix}tiktok23*
+║➵ *${prefix}tiktok24*
+║➵ *${prefix}tiktok25*
+║➵ *${prefix}tiktok26*
+║➵ *${prefix}tiktok27*
+║➵ *${prefix}tiktok28*
+║➵ *${prefix}tiktok29*
+║➵ *${prefix}tiktok30*
+║
+━━━━━━━━❪❂❫━━━━━━━━
+
+*🤖 LOSER BOT*
+_Duvidas ou afins? digite .dono_
+Criador: Wa.me/+558499034946
+
+━━━━━━━━❪❂❫━━━━━━━━
+
+ THE BEST ❤️👽`
+}
+
+exports.help = help
+
